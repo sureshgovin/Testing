eaf37a7bdd5160a0305ac554ae960156b99f6d35
sureshgovin <68013251+sureshgovin@users.noreply.github.com>
2020-07-28 17:30:32 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/hellobuild/MainActivity.java
+++ app/src/main/java/com/example/hellobuild/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //main activity for hellobuild
+        //main activity 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
