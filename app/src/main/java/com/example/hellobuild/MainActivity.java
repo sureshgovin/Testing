17a30a43555afb8fdeabf2c2bdc4d2e2ffcba2a5
sureshgovin <68013251+sureshgovin@users.noreply.github.com>
2020-07-30 12:47:40 +0530

    DEBUG BUILD

--- app/src/main/java/com/example/hellobuild/MainActivity.java
+++ app/src/main/java/com/example/hellobuild/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //main activity
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
